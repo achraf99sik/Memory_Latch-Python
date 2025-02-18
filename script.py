7e8ec892168b1bc116044da8a20767948ca3561c
achraf <achrafsikal@gmail.com>
2025-02-18 21:50:03 +0100

    improve the readability and maintainability of the code by implementing oop

--- script.py
+++ script.py
@@ -1,27 +1,32 @@
-latch_states = [False] * 8
+class Register8Bit:
+    def __init__(self):
+        self.latch_states = [0] * 8
+        self.R8in = [0] * 8
+        self.enable = False
 
-def AndOrLatch(set: bool, reset: bool, index: int) -> int:
-    global latch_states
-    latch_states[index] = (set or latch_states[index]) and not reset
-    return int(latch_states[index])
+    def and_or_latch(self, set: bool, reset: bool, index: int) -> int:
+        self.latch_states[index] = (set or self.latch_states[index]) and not reset
+        return int(self.latch_states[index])
 
-def GatedLatch(data_input: bool, write_enable: bool, index: int) -> int:
-    return AndOrLatch(data_input and write_enable, not data_input and write_enable, index)
-def Recalculate():
-    return [GatedLatch(R8in[i], enable, i) for i in range(8)]
+    def gated_latch(self, data_input: bool, write_enable: bool, index: int) -> int:
+        return self.and_or_latch(data_input and write_enable, not data_input and write_enable, index)
 
-# Tests
-R8in = [0,1,0,0,0,0,0,0]
-enable = True
+    def recalculate(self) -> list:
+        return [self.gated_latch(self.R8in[i], self.enable, i) for i in range(8)]
 
-R8out = Recalculate()
+    def update_input(self, new_input: list, enable: bool):
+        self.R8in = new_input
+        self.enable = enable
+        return self.recalculate()
 
+# Testing
+register = Register8Bit()
+
+R8out = register.update_input([0,1,0,0,0,0,0,0], True)
 print(R8out)
-R8in = [0,1,0,1,0,0,0,0]
-enable = False
-R8out = Recalculate()
+
+R8out = register.update_input([0,1,0,1,0,0,0,0], False)
 print(R8out)
-R8in = [1,1,0,1,0,1,1,1]
-enable = True
-R8out = Recalculate()
+
+R8out = register.update_input([1,1,0,1,0,1,1,1], True)
 print(R8out)
